b83973546d4f04b17cd7cd90d8b66f9ae145d83c
David Robison <david.robison@slalom.com>
2024-04-13 10:25:56 -0700

    refactor

---
+++ scripts/generate_mock_tests.ts
@@ -0,0 +1,27 @@
+import { writeFileSync } from "fs";
+
+interface Test {
+  test: string;
+  duration: number;
+}
+
+let tests: Test[] = [];
+
+for (let i = 1; i <= 100; i++) {
+  let test: Test = {
+    test: `e2e_test${i}.test.ts`,
+    duration: Math.floor(Math.random() * 10) + 1, // Random number between 1 and 10
+  };
+
+  tests.push(test);
+}
+
+let json = JSON.stringify(tests, null, 2);
+
+try {
+  writeFileSync("tests.json", json, "utf8");
+  console.log("JSON file has been saved.");
+} catch (err) {
+  console.log("An error occurred while writing JSON Object to File.");
+  console.error(err);
+}
